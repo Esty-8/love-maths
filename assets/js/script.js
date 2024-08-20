ce4ed55264b1be8d4160d6b9958be22f9f88f364
Esty-8 Bechian <x4d0cil4@students.codeinstitute.net>
2024-08-20 15:46:33 +0000

    returning the correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for(let button of buttons) {
         button.addEventListener("click", function() {
             if(this.getAttribute("data-type") === "submit"){
-                alert("clicked Submit")
+               checkAnswer();
             }  else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -23,6 +23,14 @@ document.addEventListener("DOMContentLoaded", function() {
 function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    }  else {
+        alert(`Unknown game type : ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -31,7 +39,20 @@ function checkAnswer(){
 
 }
 
+/**Gets the operands (the numbers) and the operator (plus, minus etc) 
+ * directly from the dom, and returns the correct answer.
+*/
 function calculateCorrectAnswer() {
+  let operand1 = parseInt(document.getElementById('operand1').innerText);
+  let operand2 = parseInt(document.getElementById('operand2').innerText);
+  let operator = document.getElementById("operator").innerText;
+
+  if(operator === '+') {
+    return [operand1 + operand2, "addition"];
+  } else {
+    alert(`Unimpletemented operator ${operator}`);
+    throw `Unimpletemented operator ${operator}.Aborting!`
+  }
 
 }
 
@@ -45,8 +66,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubstractQuestion() {
