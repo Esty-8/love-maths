4844b722467cdf5257ce110174e843adf465646a
Esty-8 Bechian <x4d0cil4@students.codeinstitute.net>
2024-08-21 11:10:34 +0000

    add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,9 @@ function runGame(gameType) {
 
     if(gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    }  else {
+    } else if (gameType === "multiply") {
+       displayMultiplyQuestion(num1, num2);
+    } else {
         alert(`Unknown game type : ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -66,7 +68,9 @@ function calculateCorrectAnswer() {
 
   if(operator === '+') {
     return [operand1 + operand2, "addition"];
-  } else {
+  } else if (operator === 'x'){
+    return [operand1 * operand2, "multiply"]
+   } else {
     alert(`Unimpletemented operator ${operator}`);
     throw `Unimpletemented operator ${operator}.Aborting!`
   }
@@ -94,7 +98,7 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-    
+
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
@@ -105,5 +109,7 @@ function displaySubstractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
